709ebcb88951aee2531f2ade747e499178e0900f
Yang <yl4070@rit.edu>
2022-08-28 15:22:42 -0400

    delete not-used code

--- intro.jl
+++ intro.jl
@@ -16,19 +16,3 @@ or even opening your own Pull Requests!
 )
 
 
-path = raw"F:\steam\steamapps\common\Monster Hunter World\nativePC\pl\f_equip\pl115_0000"
-
-
-for (root, dirs, files) in walkdir(path)
-
-    new_path  = replace(root, "115"=> "121")
-    for dir in dirs
-        mkdir(joinpath(new_path, dir))
-    end
-    for f in files
-        # @show f
-        old_name = joinpath(root, f)
-        new_name = replace(old_name, "115"=>"121")
-        mv(old_name, new_name)
-    end
-end
